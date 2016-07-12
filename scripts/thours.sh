8ef593a3e7352ac5941d58d4131b88c86e597ad6
Sebastian Freundt <freundt@ga-group.nl>
2016-07-12 09:24:39 +0000

    minor, do without temporary files, pipe input data inline to fread()

--- scripts/thours.sh
+++ scripts/thours.sh
@@ -1,13 +1,12 @@
 #!/bin/zsh
 
-tmp=`mktemp`
-
 if test $# -gt 0; then
 	echo "postscript('thours.ps', onefile=TRUE);"
 	for arg; do
-		thours < "${arg}" > "${tmp}"
+		echo "x <- fread('"
+		thours < "${arg}"
+		echo "', colClasses=c('factor', 'factor', 'integer'), col.names=c('day', 'hour', 'count'));"
 		cat <<EOF
-x <- fread('${tmp}', colClasses=c('factor', 'factor', 'integer'), col.names=c('day', 'hour', 'count'));
 print(ggplot(x, aes(hour, count, fill=day)) +
 geom_bar(stat='identity', width=0.8, position=position_dodge(width=0.8)) +
 scale_fill_discrete(labels=function(x)c('M','T','W','R','F','A','S')[as.numeric(x)]));
@@ -16,5 +15,3 @@ EOF
 	echo "dev.off();"
 fi \
 	| R --no-save
-
-rm "${tmp}"
