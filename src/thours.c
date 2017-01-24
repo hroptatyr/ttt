de5b3e97652371665397e4b3a86845c0be38aa3d
Sebastian Freundt <freundt@ga-group.nl>
2017-01-24 13:54:48 +0000

    minor, allow to specify rounding and modulus in thours(1)

--- src/thours.c
+++ src/thours.c
@@ -17,14 +17,22 @@
 #define MSECS	(1000)
 
 typedef size_t cnt_t;
-typedef _Decimal32 px_t;
-typedef _Decimal64 qx_t;
 typedef long unsigned int tv_t;
-#define strtopx		strtod32
-#define pxtostr		d32tostr
-#define strtoqx		strtod64
-#define qxtostr		d64tostr
 #define NOT_A_TIME	((tv_t)-1ULL)
+
+typedef struct {
+	tv_t t;
+	enum {
+		UNIT_NONE,
+		UNIT_MSECS,
+		UNIT_DAYS,
+		UNIT_MONTHS,
+		UNIT_YEARS,
+	} u;
+} tvu_t;
+
+static tvu_t intv = {60U * 60U * 24U * 7U * MSECS, UNIT_MSECS};
+static tvu_t trnd = {60U * 60U * MSECS, UNIT_MSECS};
 
 
 static __attribute__((format(printf, 1, 2))) void
@@ -90,45 +98,118 @@ out:
 	return r;
 }
 
+static tvu_t
+strtotvu(const char *str, char **endptr)
+{
+	char *on;
+	tvu_t r;
+
+	if (!(r.t = strtoul(str, &on, 10))) {
+		return (tvu_t){};
+	}
+	switch (*on++) {
+	secs:
+	case '\0':
+	case 'S':
+	case 's':
+		/* seconds, don't fiddle */
+		r.t *= MSECS;
+	msecs:
+		r.u = UNIT_MSECS;
+		break;
+
+	case 'm':
+	case 'M':
+		switch (*on) {
+		case '\0':
+			/* they want minutes, oh oh */
+			r.t *= 60UL;
+			goto secs;
+		case 's':
+		case 'S':
+			/* milliseconds it is then */
+			goto msecs;
+		case 'o':
+		case 'O':
+			r.u = UNIT_MONTHS;
+			break;
+		default:
+			goto invalid;
+		}
+		break;
+
+	case 'y':
+	case 'Y':
+		r.u = UNIT_YEARS;
+		break;
+
+	case 'h':
+	case 'H':
+		r.t *= 60U * 60U;
+		goto secs;
+	case 'd':
+	case 'D':
+		r.u = UNIT_DAYS;
+		break;
+
+	default:
+	invalid:
+		errno = 0, serror("\
+Error: unknown suffix in interval argument, must be s, m, h, d, w, mo, y.");
+		return (tvu_t){};
+	}
+	if (endptr != NULL) {
+		*endptr = on;
+	}
+	return r;
+}
+
 
 static int
 offline(void)
 {
-	/* offline mode */
-	static cnt_t m[8U * 32U];
+/* offline mode */
 	char *line = NULL;
 	size_t llen = 0UL;
 	ssize_t nrd;
+	cnt_t *m;
+	size_t nm;
 
+	/* estimate */
+	if (intv.u != UNIT_MSECS || trnd.u != UNIT_MSECS) {
+		errno = 0, serror("not implemented");
+		return -1;
+	}
+	nm = intv.t / trnd.t;
+	if (UNLIKELY((m = calloc(nm, sizeof(*m))) == NULL)) {
+		return -1;
+	}
 	while ((nrd = getline(&line, &llen, stdin)) > 0) {
 		char *on;
 		tv_t t;
-		tv_t d, h;
 
 		if (UNLIKELY((t = strtotv(line, &on)) == NOT_A_TIME)) {
 			/* got metronome cock-up */
 			continue;
 		}
-		/* convert to hour */
-		t /= 3600U * MSECS;
-		/* align to monday */
-		t -= 4U * 24U;
-		/* obtain day */
-		d = (t / 24U) % 7U + 1U;
-		/* obtain hour */
-		h = t % 24U;
+		/* move to monday */
+		t -= 4U * 24U * 60U * 60U * MSECS;
+		/* align ... */
+		t %= intv.t;
+		/* ... and round */
+		t /= trnd.t;
 		/* count */
-		m[d * 32U + h]++;
+		m[t]++;
 	}
 
-	for (size_t d = 1U; d <= 7U; d++) {
-		for (size_t h = 0U; h < 24U; h++) {
-			printf("%zu\t%02zu\t%zu\n", d, h, m[d * 32U + h]);
-		}
+	for (size_t i = 0U; i < nm; i++) {
+		printf("%zu\t%zu\n", i, m[i]);
 	}
 
 	/* finalise our findings */
 	free(line);
+	/* and finalise the counter array */
+	free(m);
 	return 0;
 }
 
@@ -139,10 +220,26 @@ int
 main(int argc, char *argv[])
 {
 	static yuck_t argi[1U];
-	int rc = 0;
+	int rc = EXIT_SUCCESS;
 
 	if (yuck_parse(argi, argc, argv) < 0) {
-		rc = 1;
+		rc = EXIT_FAILURE;
+		goto out;
+	}
+
+	if (argi->interval_arg &&
+	    !(intv = strtotvu(argi->interval_arg, NULL)).t) {
+		errno = 0, serror("\
+Error: cannot read interval argument, must be positive.");
+		rc = EXIT_FAILURE;
+		goto out;
+	}
+
+	if (argi->round_arg &&
+	    !(trnd = strtotvu(argi->round_arg, NULL)).t) {
+		errno = 0, serror("\
+Error: cannot read rounding argument, must be positive.");
+		rc = EXIT_FAILURE;
 		goto out;
 	}
 
